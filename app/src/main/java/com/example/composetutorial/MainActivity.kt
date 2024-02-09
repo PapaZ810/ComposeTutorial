e8623f6ee90f6de383249aeef41e654a7573238c
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-02-08 21:19:19 -0700

    Initial commit

---
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(Message("Message", "Jetpack Compose"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row {
+        Image (
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact Profile Picture",
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        Message("Lexi", "Hey, can I take a look at your jetpack Compose, it's great!")
+    )
+}
